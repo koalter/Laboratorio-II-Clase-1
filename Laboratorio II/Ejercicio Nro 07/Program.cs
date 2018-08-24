e442213dab4b436136955ac23dc8e12e95610f2e
koalter <heavens_drive090@hotmail.com>
2018-08-24 14:26:21 -0300

    hecho hasta el ej 7

--- Laboratorio II/Ejercicio Nro 07/Program.cs
+++ Laboratorio II/Ejercicio Nro 07/Program.cs
@@ -16,12 +16,53 @@ namespace Ejercicio_Nro_07
 
             if (DateTime.TryParse(Console.ReadLine(), out fechaNacimiento))
             {
-                int diasVividos = DateTime.Now - fechaNacimiento;
-
-                Console.WriteLine("Ha vivido {0} dias", diasVividos);
+                int fNacEnDias = fechaNacimiento.Day + DiasPorMeses(fechaNacimiento.Month) + (fechaNacimiento.Year * 365) + Bisiestos(fechaNacimiento.Year);
+                DateTime fechaActual = DateTime.Now;
+                int fActEnDias = fechaActual.Day + DiasPorMeses(fechaActual.Month) + (fechaActual.Year * 365) + Bisiestos(fechaActual.Year);
+                int diasVividos = fActEnDias - fNacEnDias;
+                Console.WriteLine("\nDias vividos: {0}", diasVividos);
+                Console.WriteLine("\nEdad estimada: {0}", diasVividos / 365);
             }
 
             Console.ReadKey();
+        }
+
+        static int Bisiestos(int year)
+        {
+            int retorno = 0;
+
+            for (float i = 1; i <= year; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    if (i % 100 == 0)
+                    {
+                        if (i % 400 == 0)
+                        {
+                            retorno++;
+                        }
+                    }
+                    else
+                    {
+                        retorno++;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+
+        static int DiasPorMeses(int month)
+        {
+            int retorno = 0;
+            int[] meses = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            for (int i = 0; i < month; i++)
+            {
+                retorno += meses[i];
+            }
+
+            return retorno;
         }
     }
 }
