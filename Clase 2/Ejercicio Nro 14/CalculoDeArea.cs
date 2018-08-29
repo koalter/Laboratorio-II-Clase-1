f4a3805d6cb842ba4c7d77d3ec14e363d5432acf
alumno <alumno>
2018-08-28 21:04:53 -0300

    Resuelto ejercicio 14

--- Clase 2/Ejercicio Nro 14/CalculoDeArea.cs
+++ Clase 2/Ejercicio Nro 14/CalculoDeArea.cs
@@ -8,6 +8,11 @@ namespace Ejercicio_Nro_14
 {
     public class CalculoDeArea
     {
+        /// <summary>
+        /// Calcula el área de un cuadrado.
+        /// </summary>
+        /// <param name="lado">Longitud del lado del cuadrado.</param>
+        /// <returns></returns>
         public static double CalcularCuadrado(double lado)
         {
             double resultado = lado * lado;
@@ -15,6 +20,12 @@ namespace Ejercicio_Nro_14
             return resultado;
         }
 
+        /// <summary>
+        /// Calcula el área de un triángulo.
+        /// </summary>
+        /// <param name="laBase">La medida de la base del triángulo.</param>
+        /// <param name="laAltura">La altura del triángulo.</param>
+        /// <returns></returns>
         public static double CalcularTriangulo(double laBase, double laAltura)
         {
             double resultado = (laBase * laAltura) / 2;
@@ -22,6 +33,11 @@ namespace Ejercicio_Nro_14
             return resultado;
         }
 
+        /// <summary>
+        /// Calcula el área de un círculo.
+        /// </summary>
+        /// <param name="radio">Radio que posee el círculo, también es la mitad del diámetro.</param>
+        /// <returns></returns>
         public static double CalcularCirculo(double radio)
         {
             double resultado = Math.PI * Math.Pow(radio, 2);
--- Clase 2/Ejercicio Nro 14/Program.cs
+++ Clase 2/Ejercicio Nro 14/Program.cs
@@ -10,6 +10,17 @@ namespace Ejercicio_Nro_14
     {
         static void Main(string[] args)
         {
+            int lado = 4;
+            Console.WriteLine("Area Cuadrado ({0} cm): {1} cm2", lado, CalculoDeArea.CalcularCuadrado(lado));
+
+            int laBase = 3;
+            int laAltura = 4;
+            Console.WriteLine("Area Triangulo ({0} * {1}): {2} cm2", laBase, laAltura, CalculoDeArea.CalcularTriangulo(laBase, laAltura));
+
+            int radio = 6;
+            Console.WriteLine("Area Circulo (R: {0}): {1:#.00} cm2", radio, CalculoDeArea.CalcularCirculo(radio));
+
+            Console.ReadKey();
         }
     }
 }
