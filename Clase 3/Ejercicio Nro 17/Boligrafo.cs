73cec01d9e971be14df7a3b0cf08e8b62ed57272
koalter <heavens_drive090@hotmail.com>
2018-09-03 16:37:14 -0300

    empece el ejercicio 17

---
+++ Clase 3/Ejercicio Nro 17/Boligrafo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_17
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        ConsoleColor color;
+        short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        /*
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            dibujo = "*";
+            SetTinta((short)(gasto * (-1)));
+            for (int i = 0; i < gasto; i++)
+            {
+                Console.Write(dibujo);
+            }
+        }
+        */
+
+        public void Recargar()
+        {
+            short tintaQueFalta = (short)(cantidadTintaMaxima - this.tinta);
+
+            SetTinta(tintaQueFalta);
+        }
+
+        void SetTinta(short tinta)
+        {
+            short auxiliar = this.tinta;
+
+            auxiliar += tinta;
+
+            if (auxiliar >= 0 && auxiliar <= cantidadTintaMaxima)
+            {
+                this.tinta = auxiliar;
+            }
+        }
+    }
+}
---
+++ Clase 3/Ejercicio Nro 17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo azul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo rojo = new Boligrafo(ConsoleColor.Red, 50);
+        }
+    }
+}
