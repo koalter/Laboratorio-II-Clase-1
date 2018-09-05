8863e2365b49c3928a703310fe64389f36b862e7
alumno <alumno>
2018-09-04 22:19:08 -0300

    Terminar Ej.Clase 5

---
+++ Clase 5/Ejercicio Clase 5/Estante.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_5
+{
+    class Estante
+    {
+        int ubicacionEstante;
+        Producto[] productos;
+
+        // Constructores
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        // END Constructores
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        /// <summary>
+        /// Muestra la información del estante consultado.
+        /// </summary>
+        /// <param name="e">Nombre del estante.</param>
+        /// <returns>Devuelve una cadena con toda la información.</returns>
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder estante = new StringBuilder();
+            int length = e.GetProductos().Length;
+            estante.AppendFormat("Estante nº {0}:\n", e.ubicacionEstante);
+            estante.AppendFormat("Cantidad de productos: {0}\n", length);
+            
+            for (int i = 0; i < length; i++)
+            {
+                estante.AppendFormat("{0}---------------\n", i+1);
+                Producto.MostrarProducto(e.GetProductos()[i]);
+            }
+            return estante.ToString();
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            int length = e.GetProductos().Length;
+            bool igualdad = false;
+            for (int i = 0; i < length; i++)
+            {
+                if (e.GetProductos()[i] == p)
+                {
+                    igualdad = true;
+                    break;
+                }
+            }
+            return igualdad;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            int length = e.GetProductos().Length;
+            int i;
+            bool hayCapacidad = false;
+            for (i = 0; i < length; i++)
+            {
+                // ARREGLAR ESTO, (string)e.GetProductos()[i] NO PUEDE SER NULL DESDE UN PRINCIPIO
+                if ((string)e.GetProductos()[i] == null)
+                {
+                    hayCapacidad = true;
+                    break;
+                }
+            }
+            if (hayCapacidad && e != p)
+            {
+                e.GetProductos()[i] = p;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if (e == p)
+            {
+                int length = e.GetProductos().Length;
+                for (int i = 0; i < length; i++)
+                {
+                    if (e.GetProductos()[i] == p)
+                    {
+                        e.GetProductos()[i] = null;
+                    }
+                }
+            }
+            return e;
+        }
+    }
+
+    class TestEstante
+    {
+        static void Main(string[] args)
+        {
+            // Creo un estante
+            Estante estante = new Estante(3, 1);
+            // Creo 4 productos
+            Producto p1 = new Producto("Pepsi", "PESDS97413", (float)10.5);
+            Producto p2 = new Producto("Coca-Cola", "COSDS55752", (float)10.5);
+            Producto p3 = new Producto("Manaos", "MASDS51292", (float)10.5);
+            Producto p4 = new Producto("Crush", "CRSDS54861", (float)10.5);
+            // Agrego los productos al estante
+            if (estante + p1)
+            {
+                Console.WriteLine("Agregó {0} {1} {2}", p1.GetMarca(), (string)p1, p1.GetPrecio());
+            }
+            else
+            {
+                Console.WriteLine("¡NO agregó {0} {1} {2}!", p1.GetMarca(), (string)p1, p1.GetPrecio());
+            }
+            if (estante + p1)
+            {
+                Console.WriteLine("Agregó {0} {1} {2}", p1.GetMarca(), (string)p1, p1.GetPrecio());
+            }
+            else
+            {
+                Console.WriteLine("¡NO agregó {0} {1} {2}!", p1.GetMarca(), (string)p1, p1.GetPrecio());
+            }
+            if (estante + p2)
+            {
+                Console.WriteLine("Agregó {0} {1} {2}", p2.GetMarca(), (string)p2, p2.GetPrecio());
+            }
+            else
+            {
+                Console.WriteLine("¡NO agregó {0} {1} {2}!", p2.GetMarca(), (string)p2, p2.GetPrecio());
+            }
+            if (estante + p3)
+            {
+                Console.WriteLine("Agregó {0} {1} {2}", p3.GetMarca(), (string)p3, p3.GetPrecio());
+            }
+            else
+            {
+                Console.WriteLine("¡NO agregó {0} {1} {2}!", p3.GetMarca(), (string)p3, p3.GetPrecio());
+            }
+            if (estante + p4)
+            {
+                Console.WriteLine("Agregó {0} {1} {2}", p4.GetMarca(), (string)p4, p4.GetPrecio());
+            }
+            else
+            {
+                Console.WriteLine("¡NO agregó {0} {1} {2}!", p4.GetMarca(), (string)p4, p4.GetPrecio());
+            }
+            // Muestro todo el estante
+            Console.WriteLine();
+            Console.WriteLine("<------------------------------------------------->");
+            Console.WriteLine(Estante.MostrarEstante(estante));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase 5/Ejercicio Clase 5/Producto.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_5
+{
+    class Producto
+    {
+        string codigoDeBarra;
+        string marca;
+        float precio;
+
+        /// <summary>
+        /// Método constructor.
+        /// </summary>
+        /// <param name="_marca">Atributo marca: string</param>
+        /// <param name="_codigo">Atributo codigoDeBarra: string</param>
+        /// <param name="_precio">Atributo precio: float</param>
+        public Producto(string _marca, string _codigo, float _precio)
+        {
+            this.codigoDeBarra = _codigo;
+            this.marca = _marca;
+            this.precio = _precio;
+        }
+
+        // Getters
+        /// <summary>
+        /// Devuelve la marca del producto.
+        /// </summary>
+        /// <returns>Devuelve la marca del producto.</returns>
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        /// <summary>
+        /// Devuelve el precio del producto.
+        /// </summary>
+        /// <returns>Devuelve el precio del producto.</returns>
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        // END Getters
+
+        /// <summary>
+        /// Muestra el producto pasado por parámetro.
+        /// </summary>
+        /// <param name="p">Producto.</param>
+        /// <returns>Devuelve una cadena con los detalles del producto.</returns>
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder detalles = new StringBuilder();
+            detalles.AppendFormat("Marca: {0}\n", p.GetMarca());
+            detalles.AppendFormat("Precio: {0}\n", p.GetPrecio());
+            detalles.AppendFormat("Código: {0}\n", (string)p);
+            return detalles.ToString();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if ((string)p1 == (string)p2 && p1.GetMarca() == p2.GetMarca())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            if (p.GetMarca() == marca)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+    }
+}
