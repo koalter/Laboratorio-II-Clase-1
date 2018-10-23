97f8c2fb6fc46183c0419d10586964179ea23598
alumno <alumno>
2018-10-22 21:42:43 -0300

    Generics y Unit Test

---
+++ Clase 17/Listas/MiLista.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    public class MiLista<T> : IEnumerable<T>
+    {
+        private T[] lista;
+
+        public int DatoDeMiLista
+        {
+            get
+            {
+                return 5;
+            }
+            set
+            {
+                if (value != 5)
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        public T this[int t]
+        {
+            get
+            {
+                return this.lista[t];
+            }
+        }
+
+        public MiLista()
+        {
+            this.lista = new T[0];
+        }
+
+        public void Add(T elemento)
+        {
+            Array.Resize(ref lista, lista.Length + 1);
+            this.lista[lista.Length - 1] = elemento;
+        }
+
+        public void Remove(T elemento)
+        {
+            if (this.lista.Length >= 1)
+            {
+                int length = this.lista.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    if (this.lista[i].Equals(elemento))
+                    {
+                        for (int j = i+1; j < length; j++)
+                        {
+                            this.lista[j - 1] = this.lista[j];
+                        }
+                        Array.Resize(ref lista, lista.Length - 1);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.lista[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get
+            {
+                int retorno = 0;
+                foreach (T item in this.lista)
+                {
+                    retorno++;
+                }
+                return retorno;
+            }
+        }
+    }
+}
---
+++ Clase 17/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Listas;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MiLista<int> a = new MiLista<int>();
+            bool exc = false;
+
+            try
+            {
+                a.DatoDeMiLista = 3;
+            }
+            catch (Exception e)
+            {
+                exc = true;
+            }
+
+            Assert.IsTrue(exc);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MiLista<int> lista = new MiLista<int>();
+            lista.Add(4);
+            lista.Add(40);
+            Assert.IsTrue(lista.Count == 2);
+            int i = 0;
+            foreach (int item in lista)
+            {
+                Assert.AreEqual(item, lista[i]);
+                i++;
+            }
+
+            MiLista<string> lista2 = new MiLista<string>();
+            lista2.Add("4");
+            lista2.Add("40");
+
+            Assert.IsTrue(lista2.Count == 2);
+        }
+    }
+}
