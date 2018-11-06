0c2f699cdfb9a0d6c55b154dd2a750def54436f7
koalter <heavens_drive090@hotmail.com>
2018-11-06 16:51:58 -0300

    Empece el ejercicio 60 de la guia

---
+++ SQL/EjercicioNro60/EjercicioNro60/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace EjercicioNro60
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblProductNumber = new System.Windows.Forms.Label();
+            this.txtProductNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(24, 40);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(201, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(21, 24);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nombre";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(24, 129);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblProductNumber
+            // 
+            this.lblProductNumber.AutoSize = true;
+            this.lblProductNumber.Location = new System.Drawing.Point(21, 63);
+            this.lblProductNumber.Name = "lblProductNumber";
+            this.lblProductNumber.Size = new System.Drawing.Size(84, 13);
+            this.lblProductNumber.TabIndex = 8;
+            this.lblProductNumber.Text = "Número de serie";
+            // 
+            // txtProductNumber
+            // 
+            this.txtProductNumber.Location = new System.Drawing.Point(24, 79);
+            this.txtProductNumber.Name = "txtProductNumber";
+            this.txtProductNumber.Size = new System.Drawing.Size(201, 20);
+            this.txtProductNumber.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 193);
+            this.Controls.Add(this.lblProductNumber);
+            this.Controls.Add(this.txtProductNumber);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblProductNumber;
+        private System.Windows.Forms.TextBox txtProductNumber;
+    }
+}
+
---
+++ SQL/EjercicioNro60/EjercicioNro60/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EjercicioNro60
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conexion;
+        SqlCommand comando;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexion = new SqlConnection("Data Source=SMILER\\SQLEXPRESS;Initial Catalog=AdventureWorks2017;Integrated Security=True");
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Connection = conexion;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bool numeroDeSerieValido = false;
+            String[] numero = txtProductNumber.Text.Split('-');
+            if (numero.Length == 2)
+            {
+                if (numero[0].Length == 2 && numero[1].Length == 4)
+                {
+                    numeroDeSerieValido = true;
+                    foreach (char letra in numero[0])
+                    {
+                        if (char.IsLetter(letra))
+                        {
+                            char.ToUpper(letra);
+                        }
+                        else
+                        {
+                            numeroDeSerieValido = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (numeroDeSerieValido && txtName.Text != String.Empty)
+            {
+                String consulta = String.Format("INSERT INTO Production.Product(Name, ProductNumber) VALUES('{0}', '{1}')", 
+                    txtName.Text, txtProductNumber.Text);
+
+                comando.CommandText = consulta;
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                MessageBox.Show("Se ha agregado un nuevo registro.");
+            }
+        }
+    }
+}
