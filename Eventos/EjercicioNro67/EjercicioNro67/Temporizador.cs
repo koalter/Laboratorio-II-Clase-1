a0113cfcd748c5b9b215d1e630221a25a6b664eb
alumno <alumno>
2018-11-06 22:12:59 -0300

    Empece con Eventos

---
+++ Eventos/EjercicioNro67/EjercicioNro67/Temporizador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace EjercicioNro67
+{
+    public sealed class Temporizador
+    {
+        Thread hilo;
+        int intervalo;
+
+        public delegate void encargadoTiempo();
+
+        #region Propiedades
+        public bool Activo
+        {
+            get;
+            set;
+        }
+        public int Intervalo
+        {
+            get
+            {
+                return this.intervalo;
+            }
+            set
+            {
+                this.intervalo = value;
+            }
+        }
+        #endregion
+        #region Metodos
+        private void Corriendo()
+        {
+
+        }
+        #endregion
+        #region Eventos
+        public event encargadoTiempo EventoTiempo;
+        #endregion
+    }
+}
